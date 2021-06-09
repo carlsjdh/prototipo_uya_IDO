353535a153b51f656f4b7395e8774af56405cf51
Carlos Delgado <alu0101016054@ull.edu.es>
2021-06-09 21:38:16 +0100

    Some features

--- docs/js/index.js
+++ docs/js/index.js
@@ -8,5 +8,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 
-
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: 'up',
+    hoverEnabled: false
+  });
+  instances.close()
+});
 
--- docs/js/task.js
+++ docs/js/task.js
@@ -1,15 +1,25 @@
 const boton = document.querySelector("#myButton");
+const loadingCSS = `<div class="preloader-wrapper small active">
+<div class="spinner-layer spinner-green-only">
+  <div class="circle-clipper left">
+    <div class="circle"></div>
+  </div><div class="gap-patch">
+    <div class="circle"></div>
+  </div><div class="circle-clipper right">
+    <div class="circle"></div>
+  </div>
+</div>`
 
 function reloadTaskView(){
   const taskView = document.querySelector("#tasksView")
-  taskView.innerHTML = "";
+  taskView.innerHTML = loadingCSS;
   database.collection("tareas").get().then((querySnapshot) => {
     let content = ``;
     querySnapshot.forEach((doc) => {
       content += `<li><div class="collapsible-header"><i class="material-icons">filter_drama</i>${doc.data().nombre}</div>`;
       content += `<div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div></li>`;
     })
-    taskView.innerHTML += content;
+    taskView.innerHTML = content;
   })
 };
 
@@ -26,6 +36,12 @@ function submitTasktoFirestore(){
       console.log("Error añadiendo contacto: ",error);
   })
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, options);
+});
+
 
 document.addEventListener("DOMContentLoaded", (event) => {
   reloadTaskView();
