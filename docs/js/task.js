a4b47f6e58fd4bc563d2578fa1576bc023577db8
Carlos Delgado <alu0101016054@ull.edu.es>
2021-06-10 18:02:33 +0100

    Tasks features

--- docs/js/task.js
+++ docs/js/task.js
@@ -1,14 +1,15 @@
 const boton = document.querySelector("#buttonAddtask");
-const loadingCSS = `<div class="preloader-wrapper small active">
-<div class="spinner-layer spinner-green-only">
-  <div class="circle-clipper left">
-    <div class="circle"></div>
-  </div><div class="gap-patch">
-    <div class="circle"></div>
-  </div><div class="circle-clipper right">
-    <div class="circle"></div>
-  </div>
-</div>`
+const loadingCSS = 
+  `<div class="preloader-wrapper small active">
+    <div class="spinner-layer spinner-green-only">
+      <div class="circle-clipper left">
+        <div class="circle"></div>
+      </div><div class="gap-patch">
+        <div class="circle"></div>
+      </div><div class="circle-clipper right">
+        <div class="circle"></div>
+      </div>
+    </div>`
 
 function reloadTaskView(){
   const today = Date.now()
@@ -18,22 +19,26 @@ function reloadTaskView(){
   database.collection("tareas").get().then((querySnapshot) => {
     let content = ``;
     querySnapshot.forEach((doc) => {
+
+      // Days left to end the task
       var date1 = new Date(doc.data().fecha);
-      console.log(date1.getTime());
       // To calculate the time difference of two dates
       var Difference_In_Time = date1.getTime() - today;
-        
       // To calculate the no. of days between two dates
       var Difference_In_Days = Math.round(Difference_In_Time / (1000 * 3600 * 24));
-      console.log(Difference_In_Days)
-      content += `<li><div class="collapsible-header"><i class="material-icons">filter_drama</i>${doc.data().nombre} - Quedan ${Difference_In_Days} días</div>`;
-      content += `<div class="collapsible-body">  <div class="section">
-      <h5>Descripción:</h5>
+      if(Difference_In_Days < 0) {
+        Difference_In_Days = "Finalizado"
+      } else {
+        Difference_In_Days = "Quedan " + Difference_In_Days + " días"
+      }
+      content += `<li tabindex="0"><div class="collapsible-header" tabindex="0"><i class="material-icons" tabindex="0">filter_drama</i>${doc.data().nombre} - ${Difference_In_Days}</div>`;
+      content += `<div class="collapsible-body" tabindex="0">  <div class="section">
+      <h5 tabindex="0">Descripción:</h5>
       <p>${doc.data().descripcion}</p>
-    </div>  <div class="section">
-    <h5>Fecha de finalización</h5>
-    <p>${doc.data().fecha}</p>
-  </div></div></li>`;
+      </div>  <div class="section">
+      <h5>Fecha de finalización</h5>
+      <p>${doc.data().fecha}</p>
+      </div></div></li>`;
     })
     taskView.innerHTML = content;
   })
@@ -44,17 +49,21 @@ function submitTasktoFirestore(){
   const name = document.querySelector("#taskName").value;
   const description = document.querySelector("#descriptionTask").value;
   const date = document.querySelector("#dateTask").value;
-  database.collection("tareas").add({
-    nombre: name,
-    descripcion: description,
-    fecha: date
-  })
-  .then((docRef) => {
-      console.log("Contacto añadido a la BD con ID: ", docRef.id);
-  })
-  .catch( (docRef) => {
-      console.log("Error añadiendo contacto: ",error);
-  })
+  if ( name == '' || description == '' || date == ''){
+    alert("Hay campos vacíos")
+  } else {
+    database.collection("tareas").add({
+      nombre: name,
+      descripcion: description,
+      fecha: date
+    })
+    .then((docRef) => {
+        console.log("Contacto añadido a la BD con ID: ", docRef.id);
+    })
+    .catch( (docRef) => {
+        console.log("Error añadiendo contacto: ",error);
+    })
+  }
 }
 
 
